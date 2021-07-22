7124a4f867f3af9281a793831ff47bcb6f32ba01
FelixBieswanger <felix.bieswanger@googlemail.com>
2021-07-22 15:11:26 +0200

    next version with own scheduling

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from Platzholder import Platzholder
 from random import randint
 import threading
 import multiprocessing
-from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
+import datetime as dt
+import time
 
-sched = BlockingScheduler()
 
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour="14", minute="37")
-def scheduled_job():
+def do_stuff(year, month, day, period):
     platzholder = Platzholder()
     lock = threading.Lock()
 
@@ -23,15 +21,15 @@ def scheduled_job():
         bot = BibBot(index, platzholder, lock)
         bot.anmelden(username="@2374109", password="63182776")
         free_seats = bot.find_free_seats(
-            jahr=str(today.year),
-            monat=str(today.month),
-            tag=str(today.day),
-            period_param="1")
+            jahr=str(year),
+            monat=str(month),
+            tag=str(day),
+            period_param=period)
         platzholder.set(bot.platz_buchen(
             free_seats[randint(0, len(free_seats))-1]))
 
     while platzholder.get() == None:
-        if datetime.today().minute == 38:
+        if datetime.today().minute == 32:
             return
 
         threads = list()
@@ -50,4 +48,33 @@ def scheduled_job():
         print(key, d[key])
 
 
-sched.start()
+while True:
+    # get time now
+    now = datetime.today()
+
+    # es ist nach der buchungssession mittags
+    if (now.hour >= 14 and now.minute >= 32) or now.hour > 14:
+        tomorrow = now + dt.timedelta(days=1)
+
+        # sleep to next day until 8.28
+        start_morgen_nächster_tag = datetime(
+            tomorrow.year, tomorrow.month, tomorrow.day, 8, 28, 0)
+
+        diff = (start_morgen_nächster_tag - datetime.today())
+        print("sleeping for", diff.seconds, "seconds")
+        time.sleep(diff.seconds)
+
+        do_stuff(year=tomorrow.year, month=tomorrow.month,
+                 day=tomorrow.day, period=0)
+
+    # es ist nach der buchungsessions morgens
+    if(now.hour >= 8 and now.minute >= 32) or now.hour > 8:
+        # sleep bis 14.28
+        start_nachmittag = datetime(
+            now.year, now.month, now.day, 14, 28, 0)
+
+        diff = (start_nachmittag - datetime.today())
+        print("sleeping for", diff.seconds, "seconds")
+        time.sleep(diff.seconds)
+
+        do_stuff(year=now.year, month=now.month, day=now.day, period=1)
