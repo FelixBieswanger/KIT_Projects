e9eb2ad3b0e8dd2181715fa49f0fed8c8e4059d3
FelixBieswanger <felix.bieswanger@googlemail.com>
2021-07-25 21:37:06 +0200

    bug fix

--- DataManager.py
+++ DataManager.py
@@ -5,7 +5,7 @@ class DataManager:
 
     def getPlatz(username):
         with open("db/"+username+".json", "r") as file:
-            return json.loads(file)
+            return json.loads(file.read())
 
     def setPlatz(username, data):
         with open("db/"+username+".json", "w") as file:
